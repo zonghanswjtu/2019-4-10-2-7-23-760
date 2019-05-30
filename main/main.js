17d1d3f3206d68e81bd137bcecfc96a8189cd110
zonghanswjtu <qazxsw87334348@gmail.com>
2019-05-30 13:36:55 +0800

    qq

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    // zonghanli
+	let m = 0;
+	let per = 0.8;
+	let waitprice = 0.25 * inputs['parkTime'];
+	if(inputs['distance'] <= 2){ 
+		m = 6+ waitprice;
+		
+	}
+	if(inputs['distance'] > 2&&inputs['distance'] <= 8){
+		m = 6 + per * (inputs['distance'] - 2) + waitprice;
+	}
+	if(inputs['distance'] > 8){
+		m = 6 + per * 6 + per * 1.5 * (inputs['distance'] - 8) + waitprice;
+	}
+    return Math.round(m);
 };
